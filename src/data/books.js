b25700aaa3ee0b4ca3e1539ec2a9756a1c247ae0
Hector Barazorda <ph.barazorda@gmail.com>
2024-01-05 15:08:33 -0500

    feat: :art: dinamic book page

---
+++ src/data/books.js
@@ -0,0 +1,17 @@
+export const booksDB = [
+  {
+    id: 'clean-code',
+    title: 'clean Code',
+    img: 'https://m.media-amazon.com/images/I/61yd7LBlw2L._SY522_.jpg'
+  },
+  {
+    id: 'javascript-avanzado',
+    title: 'JavaScript avanzado:',
+    img: 'https://m.media-amazon.com/images/I/51QBCrZAOlL._SY522_.jpg'
+  },
+  {
+    id: 'javascript-desde-cero',
+    title: 'Aprendiendo JavaScript: Desde cero hasta ECMAScript',
+    img: 'https://m.media-amazon.com/images/I/6137upbz66L._SY522_.jpg'
+  }
+]
