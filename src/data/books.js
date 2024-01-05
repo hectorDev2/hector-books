ab1920f9e4c4d586efa4d11e3dd6c06bf1091deb
Hector Barazorda <ph.barazorda@gmail.com>
2024-01-05 18:53:35 -0500

    feat: :zap: layout app books

--- src/data/books.js
+++ src/data/books.js
@@ -2,16 +2,25 @@ export const booksDB = [
   {
     id: 'clean-code',
     title: 'clean Code',
-    img: 'https://m.media-amazon.com/images/I/61yd7LBlw2L._SY522_.jpg'
+    img: 'https://m.media-amazon.com/images/I/61yd7LBlw2L._SY522_.jpg',
+    opinion:
+      'Eloquent JavaScript abarca conceptos fundamentales de JavaScript, como tipos de datos, funciones, objetos y estructuras de control, así como temas más avanzados, como el manejo de eventos, el modelo de objetos del documento (DOM) y la programación asincrónica. Además, el libro tiene ejercicios prácticos y proyectos que ayudan a los lectores a aplicar lo que han aprendido.',
+    author: 'Robert C. Martin'
   },
   {
     id: 'javascript-avanzado',
     title: 'JavaScript avanzado:',
-    img: 'https://m.media-amazon.com/images/I/51QBCrZAOlL._SY522_.jpg'
+    img: 'https://m.media-amazon.com/images/I/51QBCrZAOlL._SY522_.jpg',
+    opinion:
+      'Sumérgete en el fascinante mundo del desarrollo web con "JavaScript Avanzado". Este libro, escrito por el reconocido experto Alex CódigoMaestro, te llevará más allá de los fundamentos y te guiará a través de conceptos avanzados que transformarán tu manera de escribir código en JavaScript.',
+    author: 'Alex CodigoMaestro'
   },
   {
     id: 'javascript-desde-cero',
     title: 'Aprendiendo JavaScript: Desde cero hasta ECMAScript',
-    img: 'https://m.media-amazon.com/images/I/6137upbz66L._SY522_.jpg'
+    img: 'https://m.media-amazon.com/images/I/6137upbz66L._SY522_.jpg',
+    opinion:
+      'JavaScript Desde Cero" es la guía esencial para aquellos que desean iniciar su viaje en el emocionante mundo de la programación web con JavaScript. Escrito por la experimentada desarrolladora Laura Programadora, este libro está diseñado para principiantes absolutos y proporciona una introducción completa y práctica al lenguaje de programación más utilizado en la web.',
+    author: 'Laura Programadora'
   }
 ]
